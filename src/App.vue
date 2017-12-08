27f4b28320ce1f1d27bdf7bdb240a31c74dc20b9
Todd Vanderlin <todd@vanderlin.cc>
2017-12-08 12:09:46 -0500

    ok done - this is suppose to be stupid

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,32 @@
 <section class="hero is-fullheight">
   <div class="hero-body">
     <div class="container">
-        <div class="status" v-if="this.lastRate">
+        <div class="status">
             <div class="emoji">{{range[this.placeCount]}}</div>
-            <div class="is-text-7">${{toMoney(this.lastRate)}}</div>
+            <div class="is-text-7 latest" v-if="this.lastRate">${{toMoney(this.lastRate)}}</div>
+            
+            <div>
+                <a class="is-link enter-amount-link" v-if="!showEnterAmount && !buyPrice" @click="() => {this.showEnterAmount=!this.showEnterAmount}">what did you buy it at?</a>
+                
+                <template v-if="buyPrice">
+                    <div class="purcase-price">
+                        <span class="is-size-7">Purcase price ${{toMoney(buyPrice)}}</span><br>
+                        <a class="is-link enter-amount-link"  @click="() => {this.showEnterAmount=!this.showEnterAmount}">edit</a>
+                    </div>
+                </template>
+                
+                <div class="enter-amount" v-if="showEnterAmount">
+                    <div class="field has-addons">
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Price in BTC in USD?" v-model="buyPrice" @keyup.enter="setBuyPrice">
+                        </div>
+                        <div class="control">
+                            <a class="button is-info" @click="setBuyPrice">Set</a>
+                        </div>
+                    </div>
+                    <a class="is-link enter-amount-link" @click="cancelSetPrice">cancel</a>
+                </div>
+            </div>
         </div>
     </div>
   </div>
@@ -20,15 +43,34 @@ export default {
     props: {},
     data() {
         return {
+            inMoodMode: true,
+            showEnterAmount: false,
+            buyPrice: null,
             placeCount: 0,
             lastRate: null,
-            range: ['😍','🤑','🤗','😝','😁','😀','😐','😑','😶','😬','😕','🙁','😔','😒','😫','😤','😡'],
+            range: ['😍','🤑','🤗','😝','😛','😁','😀','🙃','😐','😑','😶','😬','😕','🙁','😔','😒','😫','😤','😡'],
             isLoadingData: false,
         }
     },
     components: {
     },
     methods: {
+        setBuyPrice(e) {
+            var str = ""+this.buyPrice;
+            var v = parseFloat( str.replace(/[^\d\.]/g,'') );
+            console.log(v);
+            this.buyPrice = v;
+            this.showEnterAmount = false;
+            if(this.lastRate) {
+                this.placeCount = parseInt((this.range.length/2));
+            }
+            this.inMoodMode = false;
+        },
+        cancelSetPrice(e) {
+            this.inMoodMode = true;
+            this.buyPrice = null;
+            this.showEnterAmount = false;
+        },
         toMoney(n) {
            return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
         },
@@ -38,20 +80,37 @@ export default {
                 this.isLoadingData = true;
                 axios.get(coinURL).then((resp) => {
                     if(resp.data.bpi) {
-                        if (this.lastRate == null) {
-                            this.placeCount = parseInt(this.range.length/2);
-                        }
-                        var newRate = resp.data.bpi.USD.rate_float;
+                        if(this.inMoodMode) {
+                            if (this.lastRate == null) {
+                                this.placeCount = parseInt(this.range.length/2);
+                            }
+                            var newRate = resp.data.bpi.USD.rate_float;
 
-                        // more money --
-                        if (newRate > this.lastRate && this.placeCount > 0) {
-                            this.placeCount --;
+                            // more money --
+                            if (newRate > this.lastRate && this.placeCount > 0) {
+                                this.placeCount --;
+                            }
+                            else if(newRate < this.lastRate && this.placeCount < this.range.length-1) {
+                                this.placeCount ++;
+                            }
+                            this.lastRate = newRate
+                            console.log('new rate', newRate);
                         }
-                        else if(newRate < this.lastRate && this.placeCount < this.range.length-1) {
-                            this.placeCount ++;
+                        else {
+                            if(this.buyPrice) {
+                                var newRate = resp.data.bpi.USD.rate_float;
+                                // more money --
+                                if (newRate > this.buyPrice && this.placeCount > 0) {
+                                    this.placeCount --;
+                                }
+                                // less money ++
+                                else if(newRate < this.buyPrice && this.placeCount < this.range.length-1) {
+                                    this.placeCount ++;
+                                }
+                                this.lastRate = newRate
+                                console.log('new rate', newRate);
+                            }
                         }
-                        this.lastRate = newRate
-                        console.log('new rate', newRate);
                     }
                     this.isLoadingData = false;
                 })
@@ -81,10 +140,27 @@ export default {
 .section {
     background-color: white;
 }
+.enter-amount-link {
+    color: rgba(0, 0, 0, 0.3);
+    font-size: 12px;
+}
 .status {
     text-align: center;
     .emoji {
         font-size: 120px;
     }
+}
+.enter-amount {
+    padding-top: 50px;
+    .field {
+        justify-content: center;
+    }
+    width: 100%;
+}
+.purcase-price {
+    padding-top: 30px;
+}
+.latest {
+    font-weight: bold;
 }
 </style>
